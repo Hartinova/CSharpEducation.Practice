b9f37ab6685cdbd82767f12af0438e6a6e22a344
Hartinova <oksanaz-84@yandex.ru>
2024-07-20 22:41:39 +0400

    Напишите программу, которая считывает из консоли ваше имя и выводит приветственное сообщение

---
+++ Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как вас зовут?");
+            string name=
+            Console.ReadLine();
+            Console.WriteLine($"Привет, {name}");
+            Console.ReadLine();
+        }
+    }
+}
