4c5f26f01ed14c25c2187d4cb7211873e1a4c8a4
Hartinova <oksanaz-84@yandex.ru>
2024-08-15 21:14:50 +0400

    Выполнена практика 4

---
+++ Practice4.Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿using MathLibrary;
+
+namespace Practice4.Task2
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      var sum = MathHelper.Sum(3.12, 5.33);
+      var minus = MathHelper.Minus(13.1, 5);
+      var multiplicate = MathHelper.Multiplicate(3.12, 5.33);
+      var device = MathHelper.Divise(3.12, 5.33);
+    }
+  }
+}
