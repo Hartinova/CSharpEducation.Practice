524ca57f3a6da141c63f397a36031abc2c4b765f
Hartinova <oksanaz-84@yandex.ru>
2024-09-21 13:03:25 +0400

    Выполнена практическая работа 4. Исключения.

---
+++ Practice6.Task2/Model/WithdrawalLimitExceededException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Practice6.Task2.Model
+{
+  /// <summary>
+  /// Ошибка превышения лимита за месяц.
+  /// </summary>
+  public class WithdrawalLimitExceededException:Exception
+  {
+    #region Конструкторы
+
+    /// <summary>
+    /// Конструктор.
+    /// </summary>
+    /// <param name="message"></param>
+    public WithdrawalLimitExceededException() : base() { }
+
+    /// <summary>
+    /// Конструктор.
+    /// </summary>
+    /// <param name="message"></param>
+    public WithdrawalLimitExceededException(string message) :base(message){ }
+
+    #endregion
+  }
+}
