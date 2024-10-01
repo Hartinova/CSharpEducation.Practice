789501556c7268d49847b09881a234e894fa1466
Hartinova <oksanaz-84@yandex.ru>
2024-10-01 22:03:53 +0400

    Выполнена практика 8. Делегаты и события

---
+++ Practice8.Task6/Model/Shop.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice8.Task6.Model
+{
+  public class Shop
+  {
+    public double Amount {  get; private set; }
+
+    public delegate void ProductHandler(Product sender, PurchaseEventArgs args);
+
+    public event ProductHandler PurchaseCompleted;
+
+    public void Purchase(Product product, int count)
+    {
+      var amount= count * product.Price;
+      this.Amount += amount;
+      this.PurchaseCompleted?.Invoke(product, new PurchaseEventArgs(product.Name, amount));
+    }
+
+    public Shop() 
+    {
+      this.Amount = 0;
+    }
+  }
+}
